6e251262dabbae1c7a7c3c0802ad0ea8dbf58812
Your Name <your-email@example.com>
2025-11-04 10:32:29 +0800

    Fix MIME type issues with .mjs extension

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/personal-web/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
+      input: './index.html',
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        format: 'es',
+        entryFileNames: `assets/[name].[hash].mjs`,
+        chunkFileNames: `assets/[name].[hash].mjs`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
   }
