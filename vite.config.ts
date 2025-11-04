188a0d55c112edfe5605b5958bc9c5b3373b8088
Your Name <your-email@example.com>
2025-11-04 10:12:58 +0800

    Update base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-web/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
+  base: './',
 })
